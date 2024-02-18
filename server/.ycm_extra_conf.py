b141cfa4a1ad90f0d320c76a178eeccbcdc0789b
Drwalin <34726800+Drwalin@users.noreply.github.com>
2024-02-18 23:12:41 +0100

    Initial commit.

---
+++ server/.ycm_extra_conf.py
@@ -0,0 +1,8 @@
+def Settings( **kwargs ):
+  return {
+    'flags': ['-x', 'c++', '-Wall', '-pedantic', '-Isrc',
+    '-Iinclude',
+    '-I../ICon7-godot-client/ICon7/bitscpp/include',
+    '-I../ICon7-godot-client/ICon7/uSockets/src/libusockets.h',
+    '-I../ICon7-godot-client/ICon7/include', '-std=c++17', '-I/usr/include'],
+  }
