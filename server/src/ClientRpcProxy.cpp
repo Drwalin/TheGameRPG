30d768c8863a5c0e585bb1891ce2c51510a2362f
Drwalin <34726800+Drwalin@users.noreply.github.com>
2024-04-27 16:10:28 +0200

    Better ByteWriter usage.

--- server/src/ClientRpcProxy.cpp
+++ server/src/ClientRpcProxy.cpp
@@ -58,23 +58,21 @@ void DeleteEntity_ForPeer(RealmServer *realm, icon7::Peer *peer,
 void SpawnEntities_ForPeer(RealmServer *realm, icon7::Peer *peer)
 {
 	std::vector<uint8_t> buffer;
-	{
-		bitscpp::ByteWriter writer(buffer);
-		writer.op(ClientRpcFunctionNames::SpawnEntities);
-		realm->queryEntityLongState.each(
-			[&](flecs::entity entity,
-				const EntityLastAuthoritativeMovementState state,
-				const EntityName name, const EntityModelName model,
-				const EntityShape shape,
-				const EntityMovementParameters movementParams) {
-				writer.op((uint64_t)entity.id());
-				writer.op(state);
-				writer.op(name);
-				writer.op(model);
-				writer.op(shape);
-				writer.op(movementParams);
-			});
-	}
+	bitscpp::ByteWriter writer(buffer);
+	writer.op(ClientRpcFunctionNames::SpawnEntities);
+	realm->queryEntityLongState.each(
+		[&](flecs::entity entity,
+			const EntityLastAuthoritativeMovementState state,
+			const EntityName name, const EntityModelName model,
+			const EntityShape shape,
+			const EntityMovementParameters movementParams) {
+			writer.op((uint64_t)entity.id());
+			writer.op(state);
+			writer.op(name);
+			writer.op(model);
+			writer.op(shape);
+			writer.op(movementParams);
+		});
 	peer->Send(std::move(buffer),
 			   icon7::FLAG_RELIABLE | icon7::FLAGS_CALL_NO_FEEDBACK);
 }
@@ -83,28 +81,26 @@ void SpawnEntities_ForPeerByIds(RealmServer *realm, icon7::Peer *peer,
 								icon7::ByteReader &reader)
 {
 	std::vector<uint8_t> buffer;
-	{
-		bitscpp::ByteWriter writer(buffer);
-		writer.op(ClientRpcFunctionNames::SpawnEntities);
-		while (reader.get_remaining_bytes() >= 8) {
-			uint64_t entityId = 0;
-			reader.op(entityId);
-			flecs::entity entity = realm->Entity(entityId);
-			if (entity.is_alive()) {
-				if (entity.has<EntityLastAuthoritativeMovementState>() &&
-					entity.has<EntityName>() && entity.has<EntityModelName>() &&
-					entity.has<EntityShape>() &&
-					entity.has<EntityMovementParameters>()) {
+	bitscpp::ByteWriter writer(buffer);
+	writer.op(ClientRpcFunctionNames::SpawnEntities);
+	while (reader.get_remaining_bytes() >= 8) {
+		uint64_t entityId = 0;
+		reader.op(entityId);
+		flecs::entity entity = realm->Entity(entityId);
+		if (entity.is_alive()) {
+			if (entity.has<EntityLastAuthoritativeMovementState>() &&
+				entity.has<EntityName>() && entity.has<EntityModelName>() &&
+				entity.has<EntityShape>() &&
+				entity.has<EntityMovementParameters>()) {
 
-					writer.op(entityId);
+				writer.op(entityId);
 
-					writer.op(
-						*entity.get<EntityLastAuthoritativeMovementState>());
-					writer.op(*entity.get<EntityName>());
-					writer.op(*entity.get<EntityModelName>());
-					writer.op(*entity.get<EntityShape>());
-					writer.op(*entity.get<EntityMovementParameters>());
-				}
+				writer.op(
+					*entity.get<EntityLastAuthoritativeMovementState>());
+				writer.op(*entity.get<EntityName>());
+				writer.op(*entity.get<EntityModelName>());
+				writer.op(*entity.get<EntityShape>());
+				writer.op(*entity.get<EntityMovementParameters>());
 			}
 		}
 	}
@@ -136,31 +132,27 @@ void Broadcast_UpdateEntities(RealmServer *realm)
 	int written = 0;
 
 	std::vector<uint8_t> buffer;
-	{
-		bitscpp::ByteWriter writer(buffer);
-		writer.op(ClientRpcFunctionNames::UpdateEntities);
+	bitscpp::ByteWriter writer(buffer);
+	writer.op(ClientRpcFunctionNames::UpdateEntities);
 
-		realm->queryLastAuthoritativeState.each(
-			[&](flecs::entity entity,
-				const EntityLastAuthoritativeMovementState &state) {
-				if (writer.GetSize() + singleEntitySize >= 1100 &&
-					written > 0) {
-					writer.~ByteWriter();
-					realm->Broadcast(buffer,
-									 icon7::FLAG_UNRELIABLE |
-										 icon7::FLAGS_CALL_NO_FEEDBACK,
-									 0);
-					buffer.clear();
-					written = 0;
-					new (&writer) bitscpp::ByteWriter(buffer);
-					writer.op(ClientRpcFunctionNames::UpdateEntities);
-				}
+	realm->queryLastAuthoritativeState.each(
+		[&](flecs::entity entity,
+			const EntityLastAuthoritativeMovementState &state) {
+			if (writer.GetSize() + singleEntitySize >= 1100 &&
+				written > 0) {
+				realm->Broadcast(buffer,
+								 icon7::FLAG_UNRELIABLE |
+									 icon7::FLAGS_CALL_NO_FEEDBACK,
+								 0);
+				buffer.clear();
+				written = 0;
+				writer.op(ClientRpcFunctionNames::UpdateEntities);
+			}
 
-				writer.op((uint64_t)entity.id());
-				writer.op(state);
-				written++;
-			});
-	}
+			writer.op((uint64_t)entity.id());
+			writer.op(state);
+			written++;
+		});
 	if (written > 0) {
 		realm->Broadcast(
 			buffer, icon7::FLAG_UNRELIABLE | icon7::FLAGS_CALL_NO_FEEDBACK, 0);
