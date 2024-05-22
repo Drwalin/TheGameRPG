cd7901ef0cec860edaa1ca1d3b197f2df3ec40e9
Drwalin <34726800+Drwalin@users.noreply.github.com>
2024-05-23 00:58:29 +0200

    Temp commit - more work towards database integration.

--- server/include/DBStatement.hpp
+++ server/include/DBStatement.hpp
@@ -4,6 +4,43 @@
 
 #include <string_view>
 
+namespace sqlite
+{
+enum ErrorCode : int {
+	OK = 0,
+	ERROR = 1,
+	INTERNAL = 2,
+	PERM = 3,
+	ABORT = 4,
+	BUSY = 5,
+	LOCKED = 6,
+	NOMEM = 7,
+	READONLY = 8,
+	INTERRUPT = 9,
+	IOERR = 10,
+	CORRUPT = 11,
+	NOTFOUND = 12,
+	FULL = 13,
+	CANTOPEN = 14,
+	PROTOCOL = 15,
+	EMPTY = 16,
+	SCHEMA = 17,
+	TOOBIG = 18,
+	CONSTRAINT = 19,
+	MISMATCH = 20,
+	MISUSE = 21,
+	NOLFS = 22,
+	AUTH = 23,
+	FORMAT = 24,
+	RANGE = 25,
+	NOTADB = 26,
+	NOTICE = 27,
+	WARNING = 28,
+	ROW = 100,
+	DONE = 101,
+};
+}
+
 struct sqlite3;
 struct sqlite3_stmt;
 
@@ -18,12 +55,17 @@ struct Statement
 	int Step();
 	int Reset();
 	
+	inline bool IsValid() const { return db && stmt; }
+	inline operator bool() const { return db && stmt; }
+	
 	int BindString(int id, std::string_view value);
 	int BindBlob(int id, std::string_view value);
 	int BindInt32(int id, int32_t value);
 	int BindInt64(int id, int64_t value);
 	int BindFloat(int id, double value);
 	int BindNull(int id);
+	
+	int ClearBindings();
 	
 	int ColumnCount();
 	std::string_view ColumnName(int id);
--- server/src/PeerStateTransitions.cpp
+++ server/src/PeerStateTransitions.cpp
@@ -1,6 +1,8 @@
 #include "../include/PeerData.hpp"
 #include "../include/RealmServer.hpp"
 #include "../include/ServerCore.hpp"
+#include "../include/DBWorker.hpp"
+#include "../include/ClientRpcProxy.hpp"
 
 #include "../include/PeerStateTransitions.hpp"
 
@@ -9,19 +11,61 @@ namespace peer_transitions
 void OnReceivedLogin(icon7::Peer *peer, const std::string &username)
 {
 	PeerData *data = ((PeerData *)(peer->userPointer));
-	
-	
-	
-	
-	
-	
+	if (data->userName != "" || username == "" || data->peer.expired()) {
+		// TODO: re-login is not available
+		LOG_INFO("Login failed: '%s' != ""  ||  '%s' == ""  ||  %s == true",
+				data->userName.c_str(), username.c_str(),
+				data->peer.expired() ? "true" : "false"
+				);
+		ClientRpcProxy::LoginFailed(peer);
+		return;
+	}
+
+	DBWorker::GetSingleton()->EnqueueCommand(
+		+[](std::weak_ptr<icon7::Peer> _peer, std::string username) {
+			auto peer = _peer.lock();
+			if (!peer) {
+				// peer already disconnected
+				return;
+			}
+			auto &db = *DBWorker::GetSingleton();
+			auto core = ((ServerCore *)(peer->host->userPointer));
+
+			static sqlite::Statement stmt = db.PrepareStatement(
+				"SELECT username, realm_name FROM Users WHERE ?;",
+				(int *)nullptr);
+
+			stmt.BindString(1, username);
+
+			int rc = stmt.Step();
+			if (rc == sqlite::ROW) {
+				// TODO: user exists in database, perform join to realm
+				std::string_view sv = stmt.ColumnString(2);
+				std::string realmName{sv.data(), sv.size()};
+// 				core->ConnectPeerToRealm(peer.get(), realmName);
+				
+				
+				
+			} else if (rc == sqlite::DONE) {
+				// TODO: no user exists in database, spawn user
+				
+				
+				
+			} else {
+				LOG_ERROR("Failed to execute user select from database: %s",
+						  db.ErrorMessage().c_str());
+			}
+			
+			
+			
+			stmt.Reset();
+			stmt.ClearBindings();
+		}, peer->weak_from_this(), username);
 	
 	
 	
 	auto core = ((ServerCore *)(peer->host->userPointer));
 	// TODO: get player data from database and call core->ConnectPeerToRealm
 	core->ConnectPeerToRealm(peer, core->spawnRealm);
-	
-	
 }
-}
+} // namespace peer_transitions
