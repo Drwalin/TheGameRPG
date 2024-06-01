a7adf1933cade15b9a56408cd8112b69ad887df6
Drwalin <34726800+Drwalin@users.noreply.github.com>
2024-06-02 00:10:47 +0200

    Correct Realm::Clear() implementation.

--- common/src/Realm.cpp
+++ common/src/Realm.cpp
@@ -23,18 +23,32 @@ void Realm::Destroy()
 void Realm::Clear()
 {
 	ecs.defer_begin();
-	ecs.each([](flecs::entity entity, EntityShape) { entity.destruct(); });
+	ecs.each([](flecs::entity entity, EntityShape &) { entity.destruct(); });
 	ecs.defer_end();
 	ecs.defer_begin();
 	ecs.each(
-		[](flecs::entity entity, EntityEventsQueue) { entity.destruct(); });
+		[](flecs::entity entity, EntityEventsQueue &) { entity.destruct(); });
 	ecs.defer_end();
 	ecs.defer_begin();
 	ecs.each(
-		[](flecs::entity entity, EntityMovementState) { entity.destruct(); });
+		[](flecs::entity entity, EntityMovementState &) { entity.destruct(); });
 	ecs.defer_end();
 	ecs.defer_begin();
-	ecs.each([](flecs::entity entity, EntityName) { entity.destruct(); });
+	ecs.each([](flecs::entity entity, EntityName &) { entity.destruct(); });
+	ecs.defer_end();
+	ecs.defer_begin();
+	ecs.each([](flecs::entity entity, EntityStaticCollisionShapeName &) {
+		entity.destruct();
+	});
+	ecs.defer_end();
+	ecs.defer_begin();
+	ecs.each([](flecs::entity entity, EntityStaticTransform &) {
+		entity.destruct();
+	});
+	ecs.defer_end();
+	ecs.defer_begin();
+	ecs.each(
+		[](flecs::entity entity, EntityModelName &) { entity.destruct(); });
 	ecs.defer_end();
 
 	collisionWorld.Clear();
