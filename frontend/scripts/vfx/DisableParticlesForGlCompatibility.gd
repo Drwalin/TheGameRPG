396fb05e8cc8577711ac73708f6ed154d418a8b2
Drwalin <34726800+Drwalin@users.noreply.github.com>
2024-07-11 12:35:17 +0200

    Disable processing for particles for gl_compatibility renderer.

--- frontend/scripts/vfx/DisableParticlesForGlCompatibility.gd
+++ frontend/scripts/vfx/DisableParticlesForGlCompatibility.gd
@@ -4,6 +4,7 @@ extends GPUParticles3D
 func _ready():
 	if ProjectSettings.get_setting("rendering/renderer/rendering_method") == "gl_compatibility":
 		visible = false;
+		process_mode = PROCESS_MODE_DISABLED;
 	#	get_parent().remove_child.call_deferred(self);
 	#	queue_free();
 	else:
