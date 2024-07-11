0593169eb7fbe1e1f953913dad934e2a4e1d6c38
Drwalin <34726800+Drwalin@users.noreply.github.com>
2024-07-11 12:30:37 +0200

    Minor corrections.

---
+++ frontend/scripts/vfx/DisableParticlesForGlCompatibility.gd
@@ -0,0 +1,18 @@
+@tool
+extends GPUParticles3D
+
+func _ready():
+	if ProjectSettings.get_setting("rendering/renderer/rendering_method") == "gl_compatibility":
+		visible = false;
+	#	get_parent().remove_child.call_deferred(self);
+	#	queue_free();
+	else:
+		visible = true;
+	global_basis = Basis();
+
+var c:int = 0;
+func _process(_dt: float)->void:
+	c = c + 1;
+	if c > 0:
+		global_basis = Basis();
+		c = -100;
