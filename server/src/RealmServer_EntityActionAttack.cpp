50f8cf9155799f1bf060cbb1e667c23bbb3f4a57
Drwalin <marek.zalewski.drwalin@gmail.com>
2024-07-21 14:27:14 +0200

    Update movement befor finalizing attack interaction.

--- server/src/RealmServer_EntityActionAttack.cpp
+++ server/src/RealmServer_EntityActionAttack.cpp
@@ -20,6 +20,8 @@ void RealmServer::Attack(uint64_t instigatorId, ComponentMovementState state,
 			auto ms = entity.get<ComponentLastAuthoritativeMovementState>();
 			if (ms) {
 				auto s = ms->oldState;
+				
+				ExecuteMovementUpdate(targetId, &s);
 
 				glm::vec3 dir =
 					glm::normalize(targetPos -
