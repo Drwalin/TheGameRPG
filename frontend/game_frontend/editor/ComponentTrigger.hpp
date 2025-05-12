b8472a394bcdcb813b36a39e1c6032d74c8129f4
Drwalin <34726800+Drwalin@users.noreply.github.com>
2025-05-12 12:25:34 +0200

    Add Teleport and Trigger editor components.

---
+++ frontend/game_frontend/editor/ComponentTrigger.hpp
@@ -0,0 +1,31 @@
+#pragma once
+
+#include <godot_cpp/classes/csg_box3d.hpp>
+
+#include "ComponentBase.hpp"
+
+namespace editor
+{
+class ComponentTrigger : public ComponentBase
+{
+	GDCLASS(ComponentTrigger, ComponentBase)
+public: // Godot bound functions
+	ComponentTrigger();
+	virtual ~ComponentTrigger();
+	static void _bind_methods();
+
+	void _ready() override;
+	void _process(double dt) override;
+
+	virtual void Serialize(icon7::ByteWriter &writer) override;
+
+public: // variables
+	String onTriggerEnter = "";
+	String get_onTriggerEnter();
+	void set_onTriggerEnter(String v);
+
+	String onTriggerExit = "";
+	String get_onTriggerExit();
+	void set_onTriggerExit(String v);
+};
+} // namespace editor
