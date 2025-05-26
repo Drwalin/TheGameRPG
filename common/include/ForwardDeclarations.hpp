5763a567cba56c16c86616f93e046544710bb9ca
Drwalin <marek.zalewski.drwalin@gmail.com>
2025-05-26 11:05:58 +0200

    Separate forward declarations file.

---
+++ common/include/ForwardDeclarations.hpp
@@ -0,0 +1,37 @@
+#pragma once
+
+namespace bitscpp
+{
+template <bool V> class ByteReader;
+template <typename T> class ByteWriter;
+} // namespace bitscpp
+
+namespace icon7
+{
+class ByteBuffer;
+class ByteReader;
+class ByteWriter;
+} // namespace icon7
+
+struct ComponentBulletCollisionObject {
+	class btCollisionObject *object;
+};
+
+class btCollisionShape;
+class btCylinderShape;
+class btCapsuleShape;
+class btSphereShape;
+class btBvhTriangleMeshShape;
+class btCollisionObject;
+
+class Realm;
+
+struct ComponentCollisionShape;
+struct ComponentStaticTransform;
+struct ComponentShape;
+
+namespace flecs
+{
+struct entity;
+};
+struct __InnerShape;
